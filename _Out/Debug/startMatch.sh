c38514bcc6709665ffc6869af9301fa31910f52b
cooado <cooado@gmail.com>
2018-07-07 15:12:04 +0800

    add ulimit

--- _Out/Debug/startMatch.sh
+++ _Out/Debug/startMatch.sh
@@ -1,1 +1,3 @@
+ulimit -n unlimited
+ulimit -c unlimited
 ./NFPluginLoader_d Server=MatchServer ID=1
--- _Out/Debug/startRole.sh
+++ _Out/Debug/startRole.sh
@@ -1,1 +1,3 @@
+ulimit -n unlimited
+ulimit -c unlimited
 ./NFPluginLoader_d Server=RoleServer ID=100
