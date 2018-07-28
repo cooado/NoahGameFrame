8084b041aa51296a8b0e03f3a51216810c0bbfc0
cooado <cooado@gmail.com>
2018-07-28 14:20:39 +0800

    tls define

--- NFComm/NFNetPlugin/NFIWS.h
+++ NFComm/NFNetPlugin/NFIWS.h
@@ -19,7 +19,7 @@
 #define ASIO_STANDALONE
 #define _WEBSOCKETPP_CPP11_STL_
 
-//#include <websocketpp/config/asio_no_tls.hpp>
+#include <websocketpp/config/asio_no_tls.hpp>
 #include <websocketpp/config/asio.hpp>
 #include <websocketpp/server.hpp>
 
@@ -60,7 +60,45 @@ struct NF_WS_CONFIG : public websocketpp::config::asio_tls {
 		websocketpp::log::alevel::none;
 };
 
+struct NF_WS_CONFIG_NO_TLS : public websocketpp::config::asio {
+	// pull default settings from our core config
+	typedef websocketpp::config::asio core;
+
+	typedef core::concurrency_type concurrency_type;
+	typedef core::request_type request_type;
+	typedef core::response_type response_type;
+	typedef core::message_type message_type;
+	typedef core::con_msg_manager_type con_msg_manager_type;
+	typedef core::endpoint_msg_manager_type endpoint_msg_manager_type;
+
+	typedef core::alog_type alog_type;
+	typedef core::elog_type elog_type;
+	typedef core::rng_type rng_type;
+	typedef core::endpoint_base endpoint_base;
+
+	static bool const enable_multithreading = true;
+
+	struct transport_config : public core::transport_config {
+		typedef core::concurrency_type concurrency_type;
+		typedef core::elog_type elog_type;
+		typedef core::alog_type alog_type;
+		typedef core::request_type request_type;
+		typedef core::response_type response_type;
+
+		static bool const enable_multithreading = true;
+	};
+
+	typedef websocketpp::transport::asio::endpoint<transport_config>
+			transport_type;
+
+	static const websocketpp::log::level elog_level =
+			websocketpp::log::elevel::none;
+	static const websocketpp::log::level alog_level =
+			websocketpp::log::alevel::none;
+};
+
 typedef websocketpp::server<NF_WS_CONFIG> NFWebSockConf;
+typedef websocketpp::server<NF_WS_CONFIG_NO_TLS> NFWebSockConfNoTLS;
 
 enum NF_WS_EVENT
 {
